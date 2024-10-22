a45cc484793523ca52779a4f1b0137766b561abf
Matheus Ed. <matheus.lafeta.moc@gmail.com>
2024-10-22 12:02:34 -0300

    .

---
+++ encryption.py
@@ -0,0 +1,40 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+
+def encrypt_message_gcm(message):
+    # Generate a random 12-byte (96-bit) nonce
+
+    # Create AES-GCM cipher
+    cipher = Cipher(
+        algorithms.AES(self.session_key),
+        modes.GCM(nonce),
+        backend=default_backend()
+    )
+    
+    encryptor = cipher.encryptor()
+
+    # Encrypt the message
+    ciphertext = encryptor.update(message.encode()) + encryptor.finalize()
+
+    # Get the GCM tag
+    tag = encryptor.tag
+
+    return (ciphertext, nonce, tag)
+
+def decrypt_message_gcm(ciphertext, nonce, tag, key):
+    # Create AES-GCM cipher
+    cipher = Cipher(
+        algorithms.AES(self.session_key),
+        modes.GCM(nonce, tag),
+        backend=default_backend()
+    )
+    
+    decryptor = cipher.decryptor()
+
+    # Decrypt the ciphertext
+    decrypted_message = decryptor.update(ciphertext) + decryptor.finalize()
+
+    return decrypted_message.decode()
